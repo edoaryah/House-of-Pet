44c311c94ed96e6a59e870a696aae9aadabd682e
Edo Arya Hermawan <edoarya2002@gmail.com>
2023-05-16 23:50:46 +0700

    Update landingpage.blade.php

--- resources/views/landingpage/landingpage.blade.php
+++ resources/views/landingpage/landingpage.blade.php
@@ -65,7 +65,7 @@
     <section id="hero-section">
         <div class="container">
             <div class="row">
-                <h1 class="display-4 text-white">UBAHHHHH</h1>
+                <h1 class="display-4 text-white">UBAHHHHH222</h1>
                 <h1 class="display-4 text-white">
                     Kami Siap Menjaga <br>
                     Hewan Kesayangan Anda
