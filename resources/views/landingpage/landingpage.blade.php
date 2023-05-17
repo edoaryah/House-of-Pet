bc32b124c127443892afd8015a42089bfc80a6e1
Edo Arya Hermawan <edoarya2002@gmail.com>
2023-05-17 12:54:02 +0700

    COBAGANTIFIX

--- resources/views/landingpage/landingpage.blade.php
+++ resources/views/landingpage/landingpage.blade.php
@@ -65,7 +65,7 @@
     <section id="hero-section">
         <div class="container">
             <div class="row">
-                <h1 class="display-4 text-white">FIXXXXX</h1>
+                <h1 class="display-4 text-white">COBA GANTI FIXXXXX</h1>
                 <h1 class="display-4 text-white">
                     Kami Siap Menjaga <br>
                     Hewan Kesayangan Anda
